9309179f7efd99ebc7c81a51947ccfe785beaa6e
InnaShamulina <innaaf@yandex.ru>
2025-07-08 22:48:09 +0300

    hw 8, without styles

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -1,20 +1,28 @@
-import {UserType} from '../HW8'
+import { UserType } from "../HW8";
 
 type ActionType =
-    | { type: 'sort'; payload: 'up' | 'down' }
-    | { type: 'check'; payload: number }
+  | { type: "sort"; payload: "up" | "down" }
+  | { type: "check"; payload: number };
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
-    switch (action.type) {
-        case 'sort': { // by name
-
-            return state // need to fix
-        }
-        case 'check': {
-
-            return state // need to fix
-        }
-        default:
-            return state
+export const homeWorkReducer = (state: UserType[], action: ActionType): any => {
+  // need to fix any
+  switch (action.type) {
+    case "sort":
+      if (action.payload === "up") {
+        console.log("up");
+        const newState = state.sort((a, b) => a.name.localeCompare(b.name));
+        return newState;
+      }
+      if (action.payload === "down") {
+        const newState2 = state.sort((a, b) => b.name.localeCompare(a.name));
+        return newState2;
+      }
+      break;
+    case "check": {
+      const newState = state.filter((u) => u.age >= action.payload);
+      return newState; // need to fix
     }
-}
+    default:
+      return state;
+  }
+};
